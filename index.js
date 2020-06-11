19641f1ec3c419005c169251cd64b5a093aa2eb0
Jessica Pope <jessica.pope.co@gmail.com>
2020-06-11 08:51:22 -0600

    6.11.2020 solution

--- index.js
+++ index.js
@@ -67,4 +67,30 @@ var countBits = function(n) {
             .length
   };
 
+//6.11.20
+const data = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+    }
+    
+    function greet(language) {
+     return !data[language] ?'Welcome': data[language] 
+    }
+
+    
 
